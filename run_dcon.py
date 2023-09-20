23a5c12f94aff0a04691c56304bd38d791b055df
sun51027 <linshih010@gmail.com>
2023-09-20 15:00:40 +0800

    add nn mode, but need to change the whole logic

--- run_dcon.py
+++ run_dcon.py
@@ -17,11 +17,12 @@ current_dir = os.getcwd()
 target_directory = "{}/output/curr_2p/dcon/".format(current_dir)
 dcon_dir = "/home/linshih/workspace/dcon_3.80/dcon_3.80/rundir/Linux"
 dcon_output = "{}/dcon_output.txt".format(current_dir)
-dcon_input = "{}/equil.in".format(dcon_dir)
+dcon_equil_in = "{}/equil.in".format(dcon_dir)
+dcon_in= "{}/dcon.in".format(dcon_dir)
 equil_file_list=[]
 
 # for local test
-#dcon_input = "{}/test.in".format(current_dir)
+#dcon_equil_in = "{}/test.in".format(current_dir)
 #dcon_dir = current_dir
 #target_directory = "{}/testdcon".format(current_dir)
 
@@ -40,26 +41,35 @@ def exe(command):
         subprocess.call(command,shell=True)
 
 def replace_equilfile(files):
-    if os.path.exists(dcon_input):
-        with open(dcon_input, "r") as fin:
+    if os.path.exists(dcon_equil_in):
+        with open(dcon_equil_in, "r") as fin:
             contents = fin.readlines()
         for index, line in enumerate(contents):
             if not 'eq_filename=' in line:
                 continue
             new_eq_filename = "\"{}\"".format(files)
             contents[index] = "        eq_filename={}\n".format(new_eq_filename)
-            with open(dcon_input, "w") as fin:
+            with open(dcon_equil_in, "w") as fin:
                  fin.writelines(contents)
             # print(contents[index])
     else:
         print("equil.in not found in {}".format(dcon_dir))
 
+def check_nn_mode(new_nn):
+    with open(dcon_in, "r") as fin:
+        contents = fin.readlines()
+    for index, line in enumerate(contents):
+        if not 'nn=' in line:
+            continue
+        contents[index] = "        nn={}\n".format(new_nn)
+        with open(dcon_in, "w") as fin:
+             fin.writelines(contents)
+
 def run_dcon():
     command = "./dcon > {}".format(dcon_output)
     exe(command)
-    #subprocess.run([dcon_command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=dcon_dir)
 
-def check_stable():
+def check_stable(nn):
     if args.dryRun:
        print("check stable for equilibria: {}".format(equil_file))
     else:
@@ -74,21 +84,22 @@ def check_stable():
              for index,line in enumerate(lines):
                  if 'qmin' not in line:
                     continue
-                 check_q_factor(line)
+                 check_q_factor(line,nn)
 
-def check_q_factor(line):
-       match = re.search(r'qmin\s*=\s*([\d.E+-]+)', line)
 
-       if match:
-          qmin_str = match.group(1)  # Extract the qmin string
-          qmin = float(qmin_str)    # Convert the qmin string to a float
-          if qmin < 1:
-             print("         qmin: {} < 1, discard it\n".format(qmin))
-          else: 
-             print("         qmin: {} > 1 is found, preserve it\n".format(qmin))
-             #print("STABLE case is found in {}".format(equil_file))
-             with open(dcon_stable_case_txt, "a") as stable_case_file:
-                  stable_case_file.write(os.path.realpath(equil_file) + "\n")
+def check_q_factor(line,nn):
+    match = re.search(r'qmin\s*=\s*([\d.E+-]+)', line)
+
+    if match:
+       qmin_str = match.group(1)  # Extract the qmin string
+       qmin = float(qmin_str)    # Convert the qmin string to a float
+       if qmin < 1:
+          print("         qmin: {} < 1, discard it\n".format(qmin))
+       else: 
+          print("         qmin: {} > 1 is found in n={} mode, preserve it\n".format(qmin,nn))
+          #print("STABLE case is found in {}".format(equil_file))
+          with open(dcon_stable_case_txt, "a") as stable_case_file:
+               stable_case_file.write(os.path.realpath(equil_file) + " with {} mode\n".format(nn))
 
     
 
@@ -100,8 +111,10 @@ if __name__=="__main__":
             equil_file = os.path.join(root, file)
             print("Running {}".format(equil_file))
             replace_equilfile(equil_file)
-            run_dcon()
-            check_stable()
+            for i in range(1,6):
+                check_nn_mode(i)
+                run_dcon()
+                check_stable(i)
 
     # Change back to the original directory
     os.chdir(current_dir)
