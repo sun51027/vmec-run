a7e3b7e5c9784dd6c3149c421ea1beb87402fa6c
sun51027 <linshih010@gmail.com>
2023-09-20 17:04:37 +0800

    check nn=1~5 & change the order of functions

--- run_dcon.py
+++ run_dcon.py
@@ -55,7 +55,7 @@ def replace_equilfile(files):
     else:
         print("equil.in not found in {}".format(dcon_dir))
 
-def check_nn_mode(new_nn):
+def replace_nn_mode(new_nn):
     with open(dcon_in, "r") as fin:
         contents = fin.readlines()
     for index, line in enumerate(contents):
@@ -75,32 +75,32 @@ def check_stable(nn):
     else:
        with open(dcon_output, "r") as dcon_output_file:
            output_contents = dcon_output_file.read()
-       if "Zero" in output_contents or "unstable" in output_contents:
-          print("....... unstable case, skipped.\n")
+       if "Zero" in output_contents or "unstable" in output_contents or "NaN" in output_contents:
+          print("......... unstable case in nn = {} mode, skipped ".format(nn))
+          return False
        else: 
-          print("....... STABLE case, check qmin:\n")
-          with open(dcon_output, "r") as dcon_output_file:
-             lines = dcon_output_file.readlines()
-             for index,line in enumerate(lines):
-                 if 'qmin' not in line:
-                    continue
-                 check_q_factor(line,nn)
-
-
-def check_q_factor(line,nn):
-    match = re.search(r'qmin\s*=\s*([\d.E+-]+)', line)
-
-    if match:
-       qmin_str = match.group(1)  # Extract the qmin string
-       qmin = float(qmin_str)    # Convert the qmin string to a float
-       if qmin < 1:
-          print("         qmin: {} < 1, discard it\n".format(qmin))
-       else: 
-          print("         qmin: {} > 1 is found in n={} mode, preserve it\n".format(qmin,nn))
-          #print("STABLE case is found in {}".format(equil_file))
+          print("......... STABLE case in nn = {} mode".format(nn))
           with open(dcon_stable_case_txt, "a") as stable_case_file:
-               stable_case_file.write(os.path.realpath(equil_file) + " with {} mode\n".format(nn))
+               stable_case_file.write(os.path.realpath(equil_file) + " in nn = {} mode\n".format(nn))
+          return True
 
+
+def check_q_factor(nn):
+    with open(dcon_output, "r") as dcon_output_file:
+        lines = dcon_output_file.readlines()
+        for index,line in enumerate(lines):
+            match = re.search(r'qmin\s*=\s*([\d.E+-]+)', line)
+            if match:
+               qmin_str = match.group(1)  # Extract the qmin string
+               qmin = float(qmin_str)    # Convert the qmin string to a float
+               if qmin < 1:
+                  print("......qmin: {} < 1, discard it".format(qmin))
+                  return False
+               else: 
+                  print("......qmin: {} > 1 is found in nn={} mode, then check stability in nn=1~5".format(qmin,nn))
+                  return True
+
+               break
     
 
 if __name__=="__main__":
@@ -109,12 +109,29 @@ if __name__=="__main__":
     for root, _, files in os.walk(target_directory):
         for file in files:
             equil_file = os.path.join(root, file)
-            print("Running {}".format(equil_file))
+            print("\nRunning {}".format(equil_file))
+            # 0.  replace equil_file
+            # 1.  run dcon in nn=1
+            # 1.5 during nn=1, check if qmin > 1, otherwise return false
+            # 2.  run dcon from nn=2 to 5
+            # 2.5 check stability 
             replace_equilfile(equil_file)
-            for i in range(1,6):
-                check_nn_mode(i)
-                run_dcon()
-                check_stable(i)
+            replace_nn_mode(1)
+            run_dcon()
+            q_larger_1= check_q_factor(1)
+            if not q_larger_1:
+                #print("q < 1, discard!")
+                continue
+            else:
+                stable = check_stable(1)
+                if not stable:
+                    continue
+                else:
+                    for i in range(2,6):
+                        replace_nn_mode(i)
+                        run_dcon()
+                        check_stable(i)
+
 
     # Change back to the original directory
     os.chdir(current_dir)
--- run_dcon_singlefile.sh
+++ run_dcon_singlefile.sh
@@ -13,12 +13,19 @@ echo "befor change path and file >>> "
 cat ${dcon_dir}/equil.in | grep eq_filename
 
 sed -i "s|eq_filename=.*|eq_filename=\"${current_dir}\/${equil_file}\"|" ${dcon_dir}/equil.in
+
 echo "after change path and file >>> "
 cat ${dcon_dir}/equil.in | grep eq_filename
 
+for n in {1..5}
+do
+	sed -i "s|nn=.*|nn=${n}|" ${dcon_dir}/dcon.in
+	./dcon 
+done
+
+
 # Run "dcon" with the current file as an argument
-./dcon 
-./xdraw crit
+#./xdraw dcon
 
 # Change back to the original directory
 #cd "$current_dir"
