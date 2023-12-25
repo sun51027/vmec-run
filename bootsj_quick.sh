c3b8b7fda88c8414a77c49ef105772f35f56e668
sun51027 <linshih010@gmail.com>
2023-12-25 17:21:37 +0800

    update iteration

--- bootsj_quick.sh
+++ bootsj_quick.sh
@@ -5,6 +5,7 @@
 #
 
 inputvmec=${1}
+iteration=${2}
 vmec_dir=$(pwd)
 vmec2tpr_dir="/home/linshih//workspace/vmec2tpr"
 bootsj="/home/linshih/workspace/terp_bootsj"
@@ -26,15 +27,17 @@ echo "Copy fort.18 to bootsj dir: ${bootsj}"
 cp fort.18 ${bootsj}
 cd ${bootsj}
 
-
 echo "Run fort43_generator.sh:"
-./fort43_generator.sh ${ns}
+./fort43_generator.sh ${inputvmec} ${ns} ${iteration}
 
 # create data file
 if [ ! -e "${inputvmec}.data" ]; then
   cp ITER_ripple.data ${inputvmec}.data
   new_value=$((ns-3))
-
+  if [ "$new_value" -lt 0 ]; then
+      echo "Error: $new_value is less than 0. Stopping the script."
+      exit 1
+  fi
   sed -i '23s/22/'"$new_value"'/' "${inputvmec}.data"
   digit_count="${#new_value}"
   if [ "$digit_count" -eq 3 ]; then
@@ -54,9 +57,9 @@ fi
 echo "Run bootsj"
 time ./tprbal.x < ${inputvmec}.data 2>&1 | tee ${inputvmec}.log
 
-#echo "Back-up fort.43 and fort.44"
-#cp fort.43 fort.43_${inputvmec}
-#cp fort.44 fort.44_${inputvmec}
+echo "Back-up fort.43 and fort.44"
+cp fort.43 fort.43_${inputvmec}
+cp fort.44 fort.44_${inputvmec}
 
 echo "[INFO] all jobs are done"
 
