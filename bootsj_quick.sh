b54b21523c482fc9049a854d1310e6d4959f2f6f
sun51027 <linshih010@gmail.com>
2024-02-16 17:27:32 +0800

    niter>2 fixing

--- bootsj_quick.sh
+++ bootsj_quick.sh
@@ -2,6 +2,9 @@
 #  Created by Lin Shih
 #  2023/12/11 
 #  VMEC -> vmec2tpr -> bootsj
+#  ./bootsj_quick.sh tcv_snake 1
+#  ./bootsj_quick.sh tcv_snake_bootsj 2 
+#  ./bootsj_quick.sh tcv_snake_bootsj2 3
 #
 
 inputvmec=${1}
@@ -11,8 +14,12 @@ vmec2tpr_dir="/home/linshih//workspace/vmec2tpr"
 bootsj="/home/linshih/workspace/terp_bootsj"
 
 
-echo "VMEC input namelist: input.${inputvmec}"
+echo "VMEC input namelist: input.${inputvmec} & wout_${inputvmec}.txt"
 #./xvmec input.${inputvmec}.vmec
+if [ ! -e "wout_$inputvmec.txt" ]; then
+  echo "Error: Input file 'wout_$inputvmec.txt' not found. Exiting script."
+  exit 1
+fi
 
 ns=$(awk 'NR==3 {print $2}' wout_${inputvmec}.txt)
 echo "ns_array from wout.txt: $ns"
@@ -30,37 +37,37 @@ cd ${bootsj}
 echo "Run fort43_generator.sh:"
 ./fort43_generator.sh ${inputvmec} ${ns} ${iteration}
 
-# create data file
-if [ ! -e "${inputvmec}.data" ]; then
-  cp ITER_ripple.data ${inputvmec}.data
-  new_value=$((ns-3))
-  if [ "$new_value" -lt 0 ]; then
-      echo "Error: $new_value is less than 0. Stopping the script."
-      exit 1
-  fi
-  sed -i '23s/22/'"$new_value"'/' "${inputvmec}.data"
-  digit_count="${#new_value}"
-  if [ "$digit_count" -eq 3 ]; then
-     echo "$new_value is 3 digital number"
-     sed -i '23s/  / /'  "${inputvmec}.data"
-  else
-     echo "$new_value is 2 digital number"
-  fi
-
-
-  short_inputvmec=$(echo "${inputvmec}" | cut -c1-7)
-  sed -i "1s/.*/               $short_inputvmec/" "${inputvmec}.data"
-  echo "New data ${inputvmec}.data file is created"
-  diff ITER_ripple.data ${inputvmec}.data
-fi
-
-echo "Run bootsj"
-time ./tprbal.x < ${inputvmec}.data 2>&1 | tee ${inputvmec}.log
-
-echo "Back-up fort.43 and fort.44"
-cp fort.43 fort.43_${inputvmec}
-cp fort.44 fort.44_${inputvmec}
-
-echo "[INFO] all jobs are done"
+##create data file
+#if [ ! -e "${inputvmec}.data" ]; then
+#  cp ITER_ripple.data ${inputvmec}.data
+#  new_value=$((ns-3))
+#  if [ "$new_value" -lt 0 ]; then
+#      echo "Error: $new_value is less than 0. Stopping the script."
+#      exit 1
+#  fi
+#  sed -i '23s/22/'"$new_value"'/' "${inputvmec}.data"
+#  digit_count="${#new_value}"
+#  if [ "$digit_count" -eq 3 ]; then
+#     echo "$new_value is 3 digital number"
+#     sed -i '23s/  / /'  "${inputvmec}.data"
+#  else
+#     echo "$new_value is 2 digital number"
+#  fi
+#
+#
+#  short_inputvmec=$(echo "${inputvmec}" | cut -c1-7)
+#  sed -i "1s/.*/               $short_inputvmec/" "${inputvmec}.data"
+#  echo "New data ${inputvmec}.data file is created"
+#  diff ITER_ripple.data ${inputvmec}.data
+#fi
+#
+#echo "Run bootsj"
+#time ./tprbal.x < ${inputvmec}.data 2>&1 | tee ${inputvmec}.log
+#
+#echo "Back-up fort.43 and fort.44"
+#cp fort.43 fort.43_${inputvmec}
+#cp fort.44 fort.44_${inputvmec}
+#
+#echo "[INFO] all jobs are done"
 
 
